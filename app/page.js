48c30b9b5ece3081109668699b52810b961a0664
Sujoy Ghosh <ghoshsujoy336@gmail.com>
2024-12-09 22:33:21 +0530

    no cache

--- app/page.js
+++ app/page.js
@@ -9,14 +9,33 @@ export default async function Page() {
   let data = [];
   let topic = "";
 
+  // try {
+  //   const response = await axios.get(
+  //     `https://brainy-quest.netlify.app/api/data`,
+  //     {
+  //       params: { _t: Date.now() }, // Adding a unique timestamp
+  //     }
+  //   );
+  //   // console.log("response: ", response.data);
+  //   data = response.data.data.questions;
+  //   topic = response.data.data.topic;
+  //   isSuccess = true;
+  // } catch (error) {
+  //   console.log("get data error: ", error);
+  //   isSuccess = "error";
+  // }
+
   try {
     const response = await axios.get(
       `https://brainy-quest.netlify.app/api/data`,
       {
-        params: { _t: Date.now() }, // Adding a unique timestamp
+        params: { _t: Date.now() }, // Adding a unique timestamp to bypass caching
+        headers: {
+          "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0", // Explicitly disable caching
+          Pragma: "no-cache", // For backward compatibility with HTTP/1.0
+        },
       }
     );
-    // console.log("response: ", response.data);
     data = response.data.data.questions;
     topic = response.data.data.topic;
     isSuccess = true;
